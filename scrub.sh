505462349c8d62077616bfe6b46a895a226e3b80
john mangual <john.mangual@gmail.com>
2015-05-14 11:10:34 -0400

    Primary and Secondary roads data set
    http://catalog.data.gov/dataset/tiger-line-shapefile-2013-series-information-file-for-the-primary-and-secondary-roads-state-bas

--- scrub.sh
+++ scrub.sh
@@ -8,3 +8,8 @@ tar -xzvf roadl_usa.shp_nt00906.tar.gz
 # chop out the roads in Puerto Rico bounding box 
 ogr2ogr -f "ESRI Shapefile" pr-roads.shp roadl_usa.shp -clipsrc -67.5  17.8 -65.1  18.6
 zip pr-roads.zip pr-roads*
+
+# Primary and Secondary roads come ready to submit to Socrata 
+# http://catalog.data.gov/dataset/tiger-line-shapefile-2013-series-information-file-for-the-primary-and-secondary-roads-state-bas
+# 
+wget http://www2.census.gov/geo/tiger/TIGER2013/PRISECROADS/tl_2013_72_prisecroads.zip
